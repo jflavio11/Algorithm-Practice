3f1a1beffe9c947828c104baf685b75b8c74d18e
Jose Flavio <jflavio90@gmail.com>
2021-03-21 19:59:11 -0500

    add reverse linked list exercise

--- src/main/kotlin/exercises/Easy.kt
+++ src/main/kotlin/exercises/Easy.kt
@@ -3,6 +3,41 @@ package exercises
 import kotlin.math.pow
 import kotlin.math.roundToInt
 
+// region Reverse a LinkedList
+// class LinkedListNode
+private class LlNode(val value: Int, var next: LlNode?)
+
+private fun printLinkedList(root: LlNode?) {
+    val ll = StringBuilder()
+    var currentLlNode: LlNode? = root
+    while (currentLlNode != null) {
+        ll.append(currentLlNode.value).append(" -> ")
+        currentLlNode = currentLlNode.next
+    }
+    ll.append("NULL")
+    println(ll)
+}
+
+/**
+ * [Animation of Reversing a LinkedList](https://media.geeksforgeeks.org/wp-content/cdn-uploads/RGIF2.gif)
+ */
+private fun reverseLinkedList(root: LlNode) {
+    print("Original LinkedList: ")
+    printLinkedList(root)
+    var previousLlNode: LlNode? = null
+    var head: LlNode? = root
+    while (head != null) {
+        val nextNode = head.next
+        head.next = previousLlNode
+        previousLlNode = head
+        head = nextNode
+    }
+    print("Reversed LinkedList: ")
+    printLinkedList(previousLlNode)
+}
+// endregion
+
+// region Perfect Number
 /**
  * Perfect number
  *
@@ -26,7 +61,7 @@ import kotlin.math.roundToInt
  * ```
  *
  **/
-fun isPerfectNumber(number: Int): Boolean {
+private fun isPerfectNumber(number: Int): Boolean {
     val factors = arrayListOf<Int>()
 
     if (number <= 0) return false
@@ -51,8 +86,12 @@ fun isPerfectNumber(number: Int): Boolean {
 
     return false
 }
+// endregion
 
 fun main() {
     val numberToCheck = 6
     println("$numberToCheck is perfect number: ${isPerfectNumber(numberToCheck)}")
+
+    val linkedList = LlNode(1, LlNode(2, LlNode(3, LlNode(4, null))))
+    reverseLinkedList(linkedList)
 }
